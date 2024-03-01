71aab342a54a4e37b2eb70f6430362b166f43deb
meidanlong <meidanlong@maoyan.com>
2024-03-01 20:48:08 +0800

    first commit

---
+++ src/main/java/org/linkgems/rical/data/Application.java
@@ -0,0 +1,16 @@
+package org.linkgems.rical.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: meidanlong
+ * @date: 2024/3/1 8:33 PM
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
