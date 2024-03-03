3d73ef8df60be2f836f4bb9944686984457b348f
meidanlong <meidanlong@maoyan.com>
2024-03-03 21:08:47 +0800

    添加数据库和mybatis依赖

--- provider/src/main/java/org/linkgems/rical/data/Application.java
+++ provider/src/main/java/org/linkgems/rical/data/Application.java
@@ -1,15 +1,18 @@
 package org.linkgems.rical.data;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @description:
+ * @description: 启动类
  * @author: meidanlong
- * @date: 2024/3/1 8:33 PM
+ * @date: 2024/3/3 10:56 AM
  */
 @SpringBootApplication
+@EnableDubbo
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
