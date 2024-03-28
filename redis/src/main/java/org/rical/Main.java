352eb7d7078593339d1e86da54cd29ac8a963c00
meidanlong <meidanlong@maoyan.com>
2024-03-28 09:50:12 +0800

    添加redisson依赖

---
+++ redis/src/main/java/org/rical/Main.java
@@ -0,0 +1,12 @@
+package org.rical;
+
+/**
+ * @description:
+ * @author: meidanlong
+ * @date: 2024/3/20 9:50 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
